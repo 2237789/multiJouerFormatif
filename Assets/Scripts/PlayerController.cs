5e52c30c97b430fce2a026153e7565f0c322947f
Crepin La Paix Hemle Djob Sotong <2237789@cegeplimoilou.ca>
2025-05-19 13:52:28 -0400

    etape 20

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("test lap");
+        
         
     }
 
